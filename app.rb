4c9f0cdbda690de003deb94bb19f28aa22b89198
davidsonz <zoe.c.m.davidson@gmail.com>
2021-11-02 18:02:43 +0000

    init

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+class Website < Sinatra::Base
+	get '/' do
+		slim :index, layout: :layout
+	end
+	get '/HowItWorks' do
+		slim :HowItWorks, layout: :layout
+	end
+	get '/EnvironmentalImpact' do
+		slim :EnvironmentalImpact, layout: :layout
+	end
+	get '/MyResearch' do
+		slim :MyResearch, layout: :layout
+	end
+end
